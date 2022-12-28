7541dc436f4df23db8f6801d2f3f49b3bf1a3bcf
Berfongel <121492984+Berfongel@users.noreply.github.com>
2022-12-28 15:08:34 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+double sayi;
+Console.WriteLine("0 dan büyük bir sayı giriniz : ");
+sayi = Convert.ToDouble(Console.ReadLine());
+//2 ile kalanı 0 ise çift değilse tek sayı
+if (sayi % 2 == 0)
+{
+    Console.WriteLine("Girdiğiniz sayı çift sayıdır.");
+}
+else
+{
+    Console.WriteLine("Girdiğiniz sayı tek sayıdır.");
+}
